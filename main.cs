75739360a0409ba905fb4b34897b948edafc99a4
T Nugent <t.nugent@hotmail.co.uk>
2022-12-01 14:01:45 +0000

    Top 3 as an array. All calculations in initial parsing loop.

--- main.cs
+++ main.cs
@@ -4,58 +4,30 @@ using System.Collections.Generic;
 class Program {
   public static void Main (string[] args) {
     string[] lines = System.IO.File.ReadAllLines(@"input.txt");
-    
-    List<int> calories = new List<int>();
 
-    foreach (string line in lines){
-      if(line.Length > 0) {
-        calories.Add(Int32.Parse(line));
-      } else {
-        calories.Add(-1);
-      }
-    }
-    calories.Add(-1);
-    
-    int[] caloriesArr = calories.ToArray();
-
-    int calories1 = 0;
-    int calories2 = 0;
-    int calories3 = 0;
+    int[] top3 = {0,0,0};
     int runningTotal = 0;
-    foreach (int calorie in caloriesArr) {
-      if (calorie == -1){
-        Console.WriteLine("==========");
-        Console.WriteLine("Running total is {0}", runningTotal);
-        Console.WriteLine("{0}, {1}, {2}", calories1, calories2, calories3);
-        if (runningTotal > calories1) {
-          calories3 = calories2;
-          calories2 = calories1;
-          calories1 = runningTotal;
-        } else if (runningTotal > calories2) {
-          calories3 = calories2;
-          calories2 = runningTotal;
-        } else if (runningTotal > calories3) {
-          calories3 = runningTotal;
+    
+    foreach (string calorie in lines){
+      if(calorie.Length > 0) {
+        runningTotal += Convert.ToInt32(calorie);
+      } else {
+        if (runningTotal > top3[0]) {
+          top3[2] = top3[1];
+          top3[1] = top3[0];
+          top3[0] = runningTotal;
+        } else if (runningTotal > top3[1]) {
+          top3[2] = top3[1];
+          top3[1] = runningTotal;
+        } else if (runningTotal > top3[2]) {
+          top3[2] = runningTotal;
         }
-        Console.WriteLine("{0}, {1}, {2}", calories1, calories2, calories3);
         runningTotal = 0;
-      }else {
-          runningTotal += calorie;
-      }
+      }    
     }
-    
-    Console.WriteLine("Top 3: {0}, {1}, {2}", calories1, calories2, calories3);
-    Console.WriteLine("The total is: {0}", calories1+calories2+calories3);
-    
-    
-
-
-
-
-
-    
     // Keep the console window open in debug mode.
-    Console.WriteLine("Press any key to exit.");
+    Console.WriteLine("Total: {0}", top3[0]+top3[1]+top3[2]);
     System.Console.ReadKey();
   }
+  
 }
